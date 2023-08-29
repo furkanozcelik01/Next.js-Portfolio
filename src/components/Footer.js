7a4d21b7c8060f3595db021b2b5dfdca4fe9d7dd
f84277176 <ferhat.ozcelik@huawei.com>
2023-08-29 14:05:14 +0300

    dark mode2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,13 +6,13 @@ const Footer = () => {
     return (
         <footer className='w-full border-t-2 border-solid
          border-dark
-        font-medium text-lg'>
+        font-medium text-lg dark:text-light dark:border-light'>
             <Layout className='py-8 flex items-center 
             justify-between'>
                 <span>{ new Date().getFullYear()} &copy; 
                 All Rights Reserved.</span>
                 <div className='flex items-center'>
-                Build with<span className='text-primary 
+                Build with<span className='text-primary dark:text-primaryDark 
                 text-2xl px-1'>&#9825;</span>
                 by&nbsp;<Link href="https://devdreaming.com"
                 className='underline underline-offset-2
@@ -20,7 +20,7 @@ const Footer = () => {
                 </div>
                 <Link href="https://devdreaming.com"
                 className='underline underline-offset-2
-                ' target={"_blank"}> Say Hello</Link>
+                ' target={"_blank"}> Say Hellwo</Link>
             </Layout>
         </footer>
     )
--- src/pages/about.js
+++ src/pages/about.js
@@ -44,7 +44,7 @@ const about = () => {
         <title>CodeBucks | about Page</title>
         <meta name="description" content="any description" />
       </Head>
-      <main className='flex w-full flex-col items-center justify-center'>
+      <main className='flex w-full flex-col items-center justify-center dark:text-light'>
         <Layout className='pt-16'>
           <AnimatedText text="Passion Fuels Purpose" className='mb-16' />
           <div className='grid w-full grid-cols-8 gap-16'>
@@ -52,7 +52,7 @@ const about = () => {
 
 
 
-              <h2 className='mb-4 text-lg font-bold uppercase text-dark/75'>Biography</h2>
+              <h2 className='mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light'>Biography</h2>
               <p className='font-medium'>
                 Hi, I'm CodeBucks, a web developer and UI/UX designer with a passion for creating beautiful, functional,
                 and user-centered digital experiences. With 4 years of experience in the field. I am always looking for
@@ -74,7 +74,7 @@ const about = () => {
             border-solid
              border-dark bg-light p-8
              ">
-              <div className='absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark'/>
+              <div className='absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:text-light'/>
               <Image src={profilePic} alt="Codebucks" className='w-full h-auto 
                 rounded-2xl '/>
             </div>
@@ -84,21 +84,21 @@ const about = () => {
                 <span className='inline-block text-7xl font-bold'>
                   <AnimatedNumbers value={50} />+
                 </span>
-                <h2 className='text-xl font-medium capitalize text-dark/75'>satisfied clients</h2>
+                <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75'>satisfied clients</h2>
               </div>
 
               <div className='flex flex-col items-end  justify-center'>
                 <span className='inline-block text-7xl font-bold'>
                   <AnimatedNumbers value={40} />+
                 </span>
-                <h2 className='text-xl font-medium capitalize text-dark/75'>project completed</h2>
+                <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75' >project completed</h2>
               </div>
 
               <div className='flex flex-col items-end  justify-center'>
                 <span className='inline-block text-7xl font-bold'>
                   <AnimatedNumbers value={4} />+
                 </span>
-                <h2 className='text-xl font-medium capitalize text-dark/75'>years of experience</h2>
+                <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75'>years of experience</h2>
               </div>
             </div>
 
